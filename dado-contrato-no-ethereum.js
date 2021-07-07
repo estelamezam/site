d20ec4611dd10fb527ce56924c497de792ff74a5
estelamezam <84424287+estelamezam@users.noreply.github.com>
2021-07-06 21:13:40 -0300

    Update dado-contrato-no-ethereum.js

--- dado-contrato-no-ethereum.js
+++ dado-contrato-no-ethereum.js
@@ -1,33 +1,32 @@
-const contractAddress = "0x3Dd7335Ee100Dc8B4f51531Cc45228A18BFd7347";
+const contractAddress = "0xCF0B65DC04483727DbbE55F86B3371c631d3700F";
 var smartContract;
 var smartContractWithSigner;
 //Both ways are valid
-//const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"paramLocator","type":"string"},{"internalType":"string","name":"paramRenter","type":"string"},{"internalType":"string","name":"paramAddressHome","type":"string"},{"internalType":"uint256","name":"paramRentalValue","type":"uint256"}],"name":"registerRental","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"rentals","outputs":[{"internalType":"string","name":"locator","type":"string"},{"internalType":"string","name":"renter","type":"string"},{"internalType":"string","name":"addressHome","type":"string"},{"internalType":"uint256","name":"rentalValue","type":"uint256"}],"stateMutability":"view","type":"function"}];
 const ABI = [
 	{
 		"inputs": [
 			{
 				"internalType": "string",
-				"name": "paramLocator",
+				"name": "paramDoador",
 				"type": "string"
 			},
 			{
 				"internalType": "string",
-				"name": "paramRenter",
+				"name": "paramDonatario",
 				"type": "string"
 			},
 			{
 				"internalType": "string",
-				"name": "paramAddressHome",
+				"name": "paramAddressDoacao",
 				"type": "string"
 			},
 			{
 				"internalType": "uint256",
-				"name": "paramRentalValue",
+				"name": "paramDonationValue",
 				"type": "uint256"
 			}
 		],
-		"name": "registerRental",
+		"name": "registerDonation",
 		"outputs": [
 			{
 				"internalType": "bool",
@@ -44,19 +43,6 @@ const ABI = [
 		"type": "constructor"
 	},
 	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
 		"inputs": [
 			{
 				"internalType": "uint256",
@@ -64,27 +50,40 @@ const ABI = [
 				"type": "uint256"
 			}
 		],
-		"name": "rentals",
+		"name": "donations",
 		"outputs": [
 			{
 				"internalType": "string",
-				"name": "locator",
+				"name": "doador",
 				"type": "string"
 			},
 			{
 				"internalType": "string",
-				"name": "renter",
+				"name": "donatario",
 				"type": "string"
 			},
 			{
 				"internalType": "string",
-				"name": "addressHome",
+				"name": "addressdoacao",
 				"type": "string"
 			},
 			{
 				"internalType": "uint256",
-				"name": "rentalValue",
+				"name": "donationValue",
 				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
 			}
 		],
 		"stateMutability": "view",
